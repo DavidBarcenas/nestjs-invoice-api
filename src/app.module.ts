ec5899b5ff0d16cafc1a17315ebca55c18d84de9
David Barcenas <daveepro@outlook.com>
2021-05-28 14:44:26 -0500

    add mongodb and create database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,21 @@ import { AppService } from './app.service';
 import { InvoiceController } from './controllers/invoice.controller';
 import { InvoiceService } from './services/invoice.service';
 import { DatabaseModule } from './database/database.module';
+import { MongoClient } from 'mongodb';
+
+const uri = `mongodb://${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/?readPreference=primary`;
+const client = new MongoClient(uri);
+
+async function run() {
+  await client.connect();
+  const database = client.db(process.env.DATABASE_NAME);
+  const invoiceCollection = database.collection(
+    process.env.DATABASE_INVOICE_COLLECTION,
+  );
+  const invoices = await invoiceCollection.find().toArray();
+}
+
+run();
 
 @Module({
   imports: [
