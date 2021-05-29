671064e2576387a76e059e2087c5c6abefeb4db0
David Barcenas <daveepro@outlook.com>
2021-05-29 11:50:18 -0500

    add missing parameters for connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,14 +17,19 @@ import config from './config';
       validationSchema: Joi.object({
         MONGO_DB: Joi.string().required(),
         MONGO_HOST: Joi.string().required(),
+        MONGO_PORT: Joi.string().required(),
+        MONGO_CONNECTION: Joi.string().required(),
       }),
     }),
     MongooseModule.forRootAsync({
       useFactory: (configService: ConfigType<typeof config>) => {
-        const { connection, host, dbName } = configService.mongo;
+        const { connection, host, dbName, user, password } =
+          configService.mongo;
         return {
           uri: `${connection}://${host}`,
           dbName,
+          user,
+          pass: password,
         };
       },
       inject: [config.KEY],
